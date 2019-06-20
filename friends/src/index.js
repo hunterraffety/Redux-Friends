931372555facb75fbdb75884590918240dec3116
hsrsbd <hsraffety@gmail.com>
2019-06-20 13:03:20 -0700

    initial commit for today. project scaffolding.

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,10 +1,35 @@
+// dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+// reducers
+import reducer from './reducers';
+
+// styles
 import './index.css';
+
+// components
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
